37faab780a58192c155355198bf76ebc1ff07e3a
Dipansh <deepanshrocks438@gmail.com>
2019-10-05 19:47:42 +0530

    Add simple navigation and add a new view controller

--- sound-perfect/ViewController.swift
+++ sound-perfect/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var startRecordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        recordLabel.text = "Tap to record !!"
+        startRecordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func startRecording(_ sender: Any) {
+        recordLabel.text = "Recording in progress ..."
+        startRecordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
 
-
+    @IBAction func stopRecording(_ sender: Any) {
+        recordLabel.text = "Tap to record !!"
+        startRecordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
 }
 
